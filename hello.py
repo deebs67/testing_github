a9a8d187641f5e97c07c1b5078f1c0010fe66e37
damianbevan <damian.bevan@real-wireless.com>
2026-08-05 14:11:53 +0100

    Added some extra commenting

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 #
-#
+# Here is a simple Python script that rolls a dice and prints the result. It uses the numpy library to
+# generate a random integer between 1 and 8 (inclusive).
 import numpy as np
 
 msg = "Roll a dice!"
